047afd5c0c542f55ba23e7b91d6ba440abbdc58d
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-08 17:02:28 +0300

    unique session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,21 @@ Create SessionAuth that inherits form Auth
 """
 from api.v1.auth.auth import Auth
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session authentication class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session for user id"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        
+        id = str(uuid4())
+        self.user_id_by_session_id[id] = user_id
+
+        return id
